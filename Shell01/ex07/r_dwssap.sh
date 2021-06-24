038c2ebdc2665e2d3c2ca7b4fe26daa21aa4d58a
danicerrato2 <73021421+danicerrato2@users.noreply.github.com>
2021-06-24 19:55:50 +0200

    Add files via upload

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | sed '/#/d' | sed -n 'n;p' | cut -d ':' -f 1 | rev | sort -r
+cat /etc/passwd | sed '/#/d' | sed -n 'n;p' | cut -d ':' -f 1 | rev | sort -r | awk 'NR < ENVIRON["FT_LINE1"] || NR > ENVIRON["FT_LINE2"] {next} {print}' | sed 's/$/,/g' | xargs | sed -e 's/,$/./g'
