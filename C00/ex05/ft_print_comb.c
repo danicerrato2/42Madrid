1787ef38ca4e68867e015b508d39eb518877390a
danicerrato2 <73021421+danicerrato2@users.noreply.github.com>
2021-06-26 20:51:09 +0200

    Add files via upload

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -6,41 +6,48 @@
 /*   By: dcerrato <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/25 20:32:11 by dcerrato          #+#    #+#             */
-/*   Updated: 2021/06/26 15:48:09 by dcerrato         ###   ########.fr       */
+/*   Updated: 2021/06/26 20:48:17 by dcerrato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 
-void	ft_print_comb(void)
+void	ft_ini_comb(char lower, char *number)
 {
-	int		i;
-	int		flag;
-	char	number[3];
+	int	i;
 
 	i = 0;
 	while (i < 3)
-		number[i] = '0' + i;
-	while (number[0] <= '7')
+	{
+		number[i] = lower + i;
+		i++;
+	}
+}
+
+void	ft_print_comb(void)
+{
+	int		i;
+	char	number[3];
+
+	ft_ini_comb('0', number);
+	while (number[0] < '9' - 2)
 	{
 		write (1, number, 3);
-		if (number[0] < '7')
-			write (1, ", ", 2);
+		write (1, ", ", 2);
 		i = 2;
-		flag = 1;
-		while (flag)
+		number[i]++;
+		while (number[i] > '9' - (2 - i) && i > 0)
 		{
-			number[i]++;
-			if (number[i] > '9' - (2 - i))
-			{
-				number[i] = number[i - 1] + 2;
-				i--;
-			}
-			else
-				flag = 0;
-		}	
+			number[i] = number[i - 1] + 2;
+			i--;
+			if (i > 0)
+				number[i]++;
+		}
+		if (i == 0)
+			ft_ini_comb(number[0] + 1, number);
 	}
+	write (1, number, 3);
 }
 
 int	main(void)
