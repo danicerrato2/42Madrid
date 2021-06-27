065a96046e1bec7506e350b176d37c5b6a26cd2b
danicerrato2 <73021421+danicerrato2@users.noreply.github.com>
2021-06-27 12:27:25 +0200

    Add files via upload

---
+++ Rush00/ex03/rush03.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcerrato <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/26 10:13:28 by dcerrato          #+#    #+#             */
+/*   Updated: 2021/06/27 12:17:56 by dcerrato         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_putchar(char c);
+
+void	ft_print_line(char first, char mid, char last, int column)
+{
+	int	j;
+
+	ft_putchar(first);
+	j = 2;
+	while (j <= column - 1)
+	{
+		ft_putchar(mid);
+		j++;
+	}
+	if (column > 1)
+		ft_putchar(last);
+}
+
+void	rush(int column, int line)
+{
+	int	i;
+
+	if ((line <= 0) || (column <= 0))
+		return ;
+	i = 1;
+	while (i <= line)
+	{
+		if (i == 1)
+			ft_print_line('A', 'B', 'C', column);
+		else if (i == line)
+			ft_print_line('A', 'B', 'C', column);
+		else
+			ft_print_line('B', ' ', 'B', column);
+		ft_putchar('\n');
+		i++;
+	}	
+}
