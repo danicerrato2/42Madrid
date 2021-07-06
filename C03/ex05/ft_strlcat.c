58ebfcd3779a60a2a47ce224e37aaefaa80ecd42
danicerrato2 <73021421+danicerrato2@users.noreply.github.com>
2021-07-06 12:21:17 +0200

    Add files via upload

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,26 +6,38 @@
 /*   By: dcerrato <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 13:45:35 by dcerrato          #+#    #+#             */
-/*   Updated: 2021/07/05 12:49:00 by dcerrato         ###   ########.fr       */
+/*   Updated: 2021/07/06 12:04:59 by dcerrato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
-	unsigned int	j;
+	unsigned int	dest_length;
+	unsigned int	src_length;
 
+	dest_length = ft_strlen(dest);
+	src_length = ft_strlen(src);
+	if (size <= 0)
+		return (src_length);
 	i = 0;
-	while (dest[i] != '\0')
-		i++;
-	j = 0;
-	while (src[j] != '\0' && j < size - i)
+	while (src[i] != '\0' && dest_length + i < size - 1)
 	{
-		dest[i + j] = src[j];
-		j++;
+		dest[dest_length + i] = src[i];
+		i++;
 	}
-	dest[i + j] = '\0';
-	while (src[j] != '\0')
-		j++;
-	return (i + j);
+	dest[dest_length + i] = '\0';
+	if (size < dest_length)
+		return (size + src_length);
+	return (dest_length + src_length);
 }
