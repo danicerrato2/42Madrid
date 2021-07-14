b2ce569f00da01f6bc0f93d6ddf0a21d70bbccdd
danicerrato2 <73021421+danicerrato2@users.noreply.github.com>
2021-07-14 12:33:46 +0200

    Add files via upload

--- C08/ex00/ft.h
+++ C08/ex00/ft.h
@@ -6,14 +6,14 @@
 /*   By: dcerrato <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/13 12:21:03 by dcerrato          #+#    #+#             */
-/*   Updated: 2021/07/13 18:18:59 by dcerrato         ###   ########.fr       */
+/*   Updated: 2021/07/14 12:33:15 by dcerrato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_H
 # define FT_H
 
-void	ft_puchar(char c);
+void	ft_putchar(char c);
 void	ft_swap(int *a, int *b);
 void	ft_putstr(char *str);
 int		ft_strlen(char *str);
