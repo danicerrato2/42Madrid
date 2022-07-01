8bf85f0f787563bb1cb05a94b4740a0f45a8094d
Daniel Cerrato sanchez <dcerrato@c3r5s6.42madrid.com>
2022-07-01 18:07:18 +0200

    Toca pulir fallos

--- Curso/printf/src/libft/ft_strncmp.c
+++ Curso/printf/src/libft/ft_strncmp.c
@@ -6,11 +6,11 @@
 /*   By: dcerrato <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 09:49:28 by dcerrato          #+#    #+#             */
-/*   Updated: 2022/07/01 13:39:22 by dcerrato         ###   ########.fr       */
+/*   Updated: 2022/07/01 16:57:24 by dcerrato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../inc/libft.h"
+#include "libft.h"
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
@@ -20,12 +20,12 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	if (i != n && s1[i] != '\0' && s2[i] == '\0')
-		return (s1[i]);
+		return ((unsigned char)s1[i]);
 	if (i != n && s1[i] == '\0' && s2[i] != '\0')
-		return (0 - s2[i]);
+		return (0 - (unsigned char)s2[i]);
 	return (0);
 }
