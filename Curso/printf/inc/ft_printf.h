3a03b4ae4d6069a2476b9c65eb6f3ee4219454e0
Daniel Cerrato sanchez <dcerrato@c1r16s1.42madrid.com>
2022-07-05 10:42:26 +0200

    Puliciones de la normi

--- Curso/printf/inc/ft_printf.h
+++ Curso/printf/inc/ft_printf.h
@@ -1,5 +1,17 @@
-#ifndef LIBFTPRINTF_H
-# define LIBFTPRINTF_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcerrato <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 10:41:26 by dcerrato          #+#    #+#             */
+/*   Updated: 2022/07/05 10:41:49 by dcerrato         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include <stdarg.h>
 # include <stdlib.h>
@@ -8,10 +20,10 @@
 int	ft_printf(const char *format, ...);
 int	ft_putchar(char c);
 int	ft_putstr(char *s);
-int ft_strlen(char *str);
-int print_digits(unsigned int n);
+int	ft_strlen(char *str);
+int	print_digits(unsigned int n);
 int	ft_putnbr(int n);
 int	print_ptr(unsigned long long ptr);
 int	print_hexa(unsigned int n, char case_type);
 
-# endif
+#endif
